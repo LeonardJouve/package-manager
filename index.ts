31d77005d1cfbbe78bfb5f706b9f7f938a299d53
Léonard Jouve <leonard.jouve@gmail.com>
2026-07-31 13:09:19 +0200

    download packages

--- index.ts
+++ index.ts
@@ -9,11 +9,13 @@ import {PackageMetadatasSchema, type PackageMetadatas} from "./metadatas.ts";
 type Dependency = {
     name: string;
     tarball: string;
-    // parent: Dependency|null;
+    version: string;
+    parents: string[];
     dependencies: Dependency[];
 };
 
 const REGISTRY = "https://registry.npmjs.org";
+const MODULE_DIRECTORY = path.join(process.cwd(), "modules");
 
 const getDependencyMetadatas = async (dependency: string): Promise<PackageMetadatas> => {
     const response = await fetch(`${REGISTRY}/${dependency}`);
@@ -22,7 +24,7 @@ const getDependencyMetadatas = async (dependency: string): Promise<PackageMetada
     return v.parse(PackageMetadatasSchema, data);
 };
 
-const getDependencies = async (dependencies: Record<string, string>, parent: Dependency|null = null) => {
+const getDependencies = async (dependencies: Record<string, string>, parents: string[] = []) => {
     return await Promise.all(Object.entries(dependencies)
         .map(async ([name, version]) => {
             const metadatas = await getDependencyMetadatas(name);
@@ -37,12 +39,13 @@ const getDependencies = async (dependencies: Record<string, string>, parent: Dep
             const dependency: Dependency = {
                 name: metadatas.name,
                 tarball: versionMetadatas.dist.tarball,
-                // parent,
+                version: chosenVersion,
+                parents,
                 dependencies: [],
             };
 
             const nestedDependencies = versionMetadatas.dependencies ?? {};
-            dependency.dependencies = await getDependencies(nestedDependencies, dependency);
+            dependency.dependencies = await getDependencies(nestedDependencies, [...parents, metadatas.name]);
 
             return dependency;
         }),
@@ -55,16 +58,73 @@ const downloadDependency = async (link: string, to: string) => {
         throw new Error(`failed to download dependency "${link}"`);
     }
 
-    const directory = path.dirname(to);
+    const tmp = await fs.mkdtemp(path.join(MODULE_DIRECTORY, "tmp-"));
 
-    await stream.pipeline(
-        response.body,
-        tar.x({C: directory}),
-    );
+    await stream.pipeline(response.body, tar.x({C: tmp}));
+    await fs.mkdir(path.dirname(to), {recursive: true});
+    await fs.rename(path.join(tmp, "package"), to);
+    await fs.rm(tmp, {recursive: true, force: true});
+};
 
-    await fs.rename(path.join(directory, "package"), to);
+const groupDependencies = (dependencies: Dependency[]): Map<string, Dependency[]> => {
+    const result = new Map<string, Dependency[]>();
+    const add = (name: string, dependency: Dependency) => {
+        if (!result.has(name)) {
+            result.set(name, []);
+        }
+
+        result.get(name)?.push(dependency);
+    };
+
+    for (const dependency of dependencies) {
+        add(dependency.name, dependency);
+        const nestedDependencies = groupDependencies(dependency.dependencies);
+        nestedDependencies.forEach((dependencies, name) => {
+            dependencies.forEach((dependency) => add(name, dependency));
+        });
+    }
+
+    return result;
+};
+
+type Plan = {
+    dependency: Dependency;
+    output: string;
+}[];
+const getDownloadPlan = (dependencies: Dependency[]): Plan => {
+    const plan: Plan = [];
+
+    groupDependencies(dependencies).forEach((versions, name) => {
+        const [dependency, ...rest] = versions;
+        if (!dependency) {
+            return;
+        }
+
+        plan.push({dependency, output: path.join(MODULE_DIRECTORY, name)});
+        rest.forEach((dependency) => {
+            plan.push({
+                dependency,
+                output: path.join(
+                    MODULE_DIRECTORY,
+                    ...dependency.parents.flatMap((parent) => [parent, path.basename(MODULE_DIRECTORY)]),
+                    name
+                ),
+            });
+        });
+    });
+
+    return plan.sort((a, b) => a.dependency.parents.length - b.dependency.parents.length);
 };
 
 const packages = JSON.parse(await fs.readFile("./packages.json", "utf-8"));
-const metadatas = await getDependencies(packages.dependencies);
-console.log(JSON.stringify(metadatas, null, 2));
+
+const tree = await getDependencies(packages.dependencies);
+
+await fs.rm(MODULE_DIRECTORY, {recursive: true, force: true});
+await fs.mkdir(MODULE_DIRECTORY);
+
+const plan = getDownloadPlan(tree);
+for (const {dependency, output} of plan) {
+    console.log(`downloading ${dependency.name}@${dependency.version} in ${output}`);
+    await downloadDependency(dependency.tarball, output);
+}
